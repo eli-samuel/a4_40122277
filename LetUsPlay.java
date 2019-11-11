7a1b1f2ea2d218e647d2739b069921fbec766b5f
eli-samuel <0.0elisamuel@gmail.com>
2019-11-11 02:49:53 -0500

    Finished user set dimensions

--- LetUsPlay.java
+++ LetUsPlay.java
@@ -16,34 +16,39 @@ public class LetUsPlay {
                             + "\n\t\t0 to use the default board size"
                             + "\n\t\t-1 to enter your own board size"
                             + "\n→ What do you want to do? ");
-        int userBoard = input.nextInt();
 
-        if (userBoard == 0) {
-            board = new Board();
-        }
-        else if (userBoard == -1) {
-            int numLevels, numSize;
-            System.out.print("How many levels would you like? (minimum size 3, max 10) ");
-            numLevels = input.nextInt();
 
-            while (numLevels > 10 || numLevels < 3) {
-                System.out.print("\nSorry but " + numLevels + " is not a legal choice. Try again: ");
+        while (true) {
+            int userBoard = input.nextInt();
+            if (userBoard == 0) {
+                board = new Board();
+                break;
+            }
+            else if (userBoard == -1) {
+                int numLevels, numSize;
+                System.out.print("How many levels would you like? (minimum size 3, max 10) ");
                 numLevels = input.nextInt();
-            }
 
-            System.out.print("What size do you want the nxn boards on each level to be? (minimum size 4x4, max 10x10) "
-                                    + "\n→ enter the value of n: ");
-            numSize = input.nextInt();
+                while (numLevels > 10 || numLevels < 3) {
+                    System.out.print("Sorry but " + numLevels + " is not a legal choice. Try again: ");
+                    numLevels = input.nextInt();
+                }
 
-            while (numSize > 10 || numSize < 4) {
-                System.out.print("\nSorry but " + numSize + " is not a legal choice. Try again: ");
+                System.out.print("What size do you want the nxn boards on each level to be? (minimum size 4x4, max 10x10) "
+                                        + "\n→ enter the value of n: ");
                 numSize = input.nextInt();
-            }
 
-            board = new Board(numLevels, numSize);
-        }
-        else {
-            board = new Board();
+                while (numSize > 10 || numSize < 4) {
+                    System.out.print("Sorry but " + numSize + " is not a legal choice. Try again: ");
+                    numSize = input.nextInt();
+                }
+
+                board = new Board(numLevels, numSize);
+                break;
+            }
+            else {
+                System.out.print("Sorry, but " + userBoard + " is not a legal choice. Try again: ");
+            }
         }
 
         System.out.println(board);
